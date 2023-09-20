fca054f20ba30f741126ea6cacf6d20da115c068
gumball373 <gmble.trey@gmail.com>
2023-09-20 09:47:16 -0500

    funtioning stripe cart

--- src/pages/api/stripe.js
+++ src/pages/api/stripe.js
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 
 export async function POST({ params, request }) {
 
-    const STRIPE_SECRET_KEY = import.meta.env.STRIPE_SECRET_KEY;
+    const STRIPE_SECRET_KEY = await db(import.meta.env.STRIPE_SECRET_KEY);
     // const STRIPE_PUBLISHABLE_KEY = import.meta.env.STRIPE_PUBLISHABLE_KEY;
 
     //require('stripe')(STRIPE_SECRET_KEY);
@@ -10,7 +10,7 @@ export async function POST({ params, request }) {
         apiVersion: '2023-08-16',
     });
 
-    // return new Response(JSON.stringify({message: STRIPE_SECRET_KEY}))
+    return new Response(JSON.stringify({message: STRIPE_SECRET_KEY}))
 
     try{
         const session = await stripe.checkout.sessions.create({
